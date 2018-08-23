659ae84bd945998bea3e0591eb97d1896e4ebcf2
Wasin Thonkaew <haxpor@gmail.com>
2018-08-24 04:49:20 +0800

    Add 12 - Color modulation

---
+++ 12_colorModulation/LTexture.c
@@ -0,0 +1,91 @@
+#include "LTexture.h"
+#include <SDL2/SDL.h>
+#include "common.h"
+#include <stdlib.h>
+
+// variables defined in common.h
+extern SDL_Window* gWindow;
+extern SDL_Renderer* gRenderer;
+
+bool LTexture_LoadFromFile(const char* path, LTexture* out)
+{
+	// we have no need to free existing texture loaded
+	// as it's not OOP, we don't hold anything for anyone
+
+	// load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load(path);
+	if (loadedSurface == NULL)
+	{
+		printf("Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError());
+		return false;
+	}
+
+	// color key image
+	SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+
+	// create texture from surface
+	SDL_Texture* newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+	if (newTexture == NULL)
+	{
+		printf("Unable to create texture from %s! SDL Error: %s\n", path, SDL_GetError());
+		// free surface
+		SDL_FreeSurface(loadedSurface);
+		return false;
+	}
+	else
+	{
+		// get image dimension
+		out->width = loadedSurface->w;
+		out->height = loadedSurface->h;
+		// set texture to ltexture
+		out->texture = newTexture;
+	}
+
+	return true;
+}
+
+void LTexture_Render(LTexture* texture, int x, int y)
+{
+	if (texture == NULL)
+	{
+		return;
+	}
+
+	// set rendering space and render to screen
+	SDL_Rect renderQuad = { x, y, texture->width, texture->height };	
+	SDL_RenderCopy(gRenderer, texture->texture, NULL, &renderQuad);
+}
+
+void LTexture_ClippedRender(LTexture* ltexture, int x, int y, SDL_Rect* clip)
+{
+	if (ltexture == NULL || clip == NULL)
+		return;
+
+	// set rendering space and render to screen
+	SDL_Rect renderQuad = { x, y, clip->w, clip->h };
+	// render to screen
+	SDL_RenderCopy(gRenderer, ltexture->texture, clip, &renderQuad);
+}
+
+void LTexture_SetColor(LTexture* ltexture, Uint8 red, Uint8 green, Uint8 blue)
+{
+	// set color for modulation
+	SDL_SetTextureColorMod(ltexture->texture, red, green, blue);
+}
+
+void LTexture_Free(LTexture* ltexture)
+{
+	// if texture exists then deallocate it first
+	if (ltexture != NULL)
+	{
+		// destroy texture as attached to its texture
+		if (ltexture->texture != NULL)
+		{
+			SDL_DestroyTexture(ltexture->texture);
+			ltexture->texture = NULL;
+		}
+
+		ltexture->width = 0;
+		ltexture->height = 0;
+	}
+}
---
+++ 12_colorModulation/common.c
@@ -0,0 +1,4 @@
+#include "common.h"
+
+SDL_Window* gWindow = NULL;
+SDL_Renderer* gRenderer = NULL;
