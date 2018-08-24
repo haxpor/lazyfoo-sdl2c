570201a139c04afb9858b3f89969b3c3479b18f9
Wasin Thonkaew <haxpor@gmail.com>
2018-08-24 15:06:33 +0800

    Add 14 - Animated Sprites and VSync

---
+++ 14_animatedSpritesVsync/LTexture.h
@@ -0,0 +1,45 @@
+/*
+ * LTexture
+ *
+ * It represents a texture wrapper with functionality related to it.
+ */
+
+#ifndef LTexture_h_
+#define LTexture_h_
+
+#include <stdbool.h>
+#include <SDL2/SDL_image.h>
+
+// i know guys, we will care about byte alignment of struct later ;)
+struct LTexture {
+	SDL_Texture* texture;
+	int width;
+	int height;
+};
+typedef struct LTexture LTexture;
+
+// load image at specified path (created on stack)
+// then return resulted via out.
+// Return true if loading succeeds, otherwise return false.
+extern bool LTexture_LoadFromFile(const char* path, LTexture* out);
+
+// render texture at given point
+extern void LTexture_Render(LTexture* texture, int x, int y);
+
+// render clipped texture at the given point
+extern void LTexture_ClippedRender(LTexture* texture, int x, int y, SDL_Rect* clip);
+
+// set color modulation
+extern void LTexture_SetColor(LTexture* ltexture, Uint8 red, Uint8 green, Uint8 blue);
+
+// set blending mode
+extern void LTexture_SetBlendMode(LTexture* ltexture, SDL_BlendMode blending);
+
+// set alpha
+extern void LTexture_SetAlpha(LTexture* ltexture, Uint8 alpha);
+
+// clear internal resource as used by LTexture
+// caller still need to free() LTexture which is texture in this case if it was dynamically created on heap
+extern void LTexture_Free(LTexture* texture);
+
+#endif /* LTexture_h_ */
---
+++ 14_animatedSpritesVsync/animatedsv.c
@@ -0,0 +1,191 @@
+/**
+ * 14 - Animated Sprites and VSync
+ *
+ */
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "common.h"
+#include "LTexture.h"
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define SETFRAME(var, arg1, arg2, arg3, arg4)		\
+		do {							\
+			var.x = arg1;					\
+			var.y = arg2;					\
+			var.w = arg3;					\
+			var.h = arg4;					\
+		} while(0)
+
+bool init();
+bool loadMedia();
+void close();
+
+// refer to variables declared and defined elsewhere (in common.h)
+extern SDL_Window* gWindow;
+extern SDL_Renderer* gRenderer;
+
+// our textures and animation
+const int kWalkingAnimationFrames = 4;
+SDL_Rect gAnimationFrames[kWalkingAnimationFrames];
+LTexture gSpriteSheetTexture;
+
+bool init() {
+	// initialize sdl
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return false;
+	}
+	
+	// create window
+	gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (gWindow == NULL) {
+		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	// create renderer for window
+	// as we use SDL_Texture, now we need to use renderer to render stuff
+	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (gRenderer == NULL)
+	{
+		printf("SDL could not create renderer! SDL_Error: %s\n", SDL_GetError());
+		return false;
+	}
+	
+	// initialize png loading
+	// see https://www.libsdl.org/projects/SDL_image/docs/SDL_image.html#SEC8
+	// returned from IMG_Init is all flags initted, so we could check for all possible
+	// flags we aim for
+	int imgFlags = IMG_INIT_PNG;
+	int inittedFlags = IMG_Init(imgFlags);
+	if ( (inittedFlags & imgFlags) != imgFlags)
+	{
+		// from document, not always that error string from IMG_GetError() will be set
+		// so don't depend on it, just for pure information
+		printf("SDL_Image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+		return false;
+	}
+	
+	return true;
+}
+
+bool loadMedia()
+{
+	// load texture
+	if (!LTexture_LoadFromFile("foo.png", &gSpriteSheetTexture))
+	{
+		printf("Failed to load foreground texture!\n");
+		return false;
+	}
+
+	// set animation frames (as clips)
+	SETFRAME(gAnimationFrames[0], 0, 0, 64, 205);
+	SETFRAME(gAnimationFrames[1], 64, 0, 64, 205);
+	SETFRAME(gAnimationFrames[2], 128, 0, 64, 205);
+	SETFRAME(gAnimationFrames[3], 192, 0, 64, 205);
+
+	return true;
+}
+
+void close()
+{
+	// clear resource of our textures
+	// if gTexture is allocated on heap via malloc() then
+	// we have to manually free() after the following call as well.
+	LTexture_Free(&gSpriteSheetTexture);
+
+	// destroy window
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	// quit SDL_image
+	IMG_Quit();
+
+	// quit sdl subsystem
+	SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+	// start up SDL and create window
+	if (!init())
+	{
+		printf("Failed to initialize\n");
+	}	
+	else
+	{
+		// load media
+		if (!loadMedia())
+		{
+			printf("Failed to load media!\n");
+		}
+		else
+		{
+			// main loop flag
+			bool quit = false;
+			
+			// event handler
+			SDL_Event e;
+
+			// current frame
+			int frame = 0;
+
+			// while application is running
+			while (!quit)
+			{
+				// handle events on queue
+				// if it's 0, then it has no pending event
+				// we keep polling all event in each game loop until there is no more pending one left
+				while (SDL_PollEvent(&e) != 0)
+				{
+					// user requests quit
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+					// user presses a key
+					else if (e.type == SDL_KEYDOWN)
+					{
+						switch (e.key.keysym.sym)
+						{
+						case SDLK_ESCAPE:
+							quit = true;
+							break;
+						}
+					}
+				}
+
+				// clear screen
+				SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0xff, 0xff);
+				SDL_RenderClear(gRenderer);
+
+				// render walk animation
+				SDL_Rect* currentFrame = &gAnimationFrames[frame/4];
+				LTexture_ClippedRender(&gSpriteSheetTexture, (SCREEN_WIDTH - currentFrame->w)/2, (SCREEN_HEIGHT - currentFrame->h)/2, currentFrame);
+
+				// update screen from any rendering performed since this previous call
+				// as we don't use SDL_Surface now, we can't use SDL_UpdateWindowSurface
+				SDL_RenderPresent(gRenderer);
+
+				// go to next frame
+				++frame;
+
+				// cycle animation
+				if (frame / 4 >= kWalkingAnimationFrames)
+				{
+					frame = 0;
+				}
+			}
+		}
+	}
+
+	// free resource and close SDL
+	close();
+
+	return 0;
+}
